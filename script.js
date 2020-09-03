3a3663acdb124cc51a3384597bbe2b9277ed388d
Pavel Klecansky <klecanskypavel@gmail.com>
2020-09-03 14:21:42 +0200

    Basic Complete

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const container = document.querySelector("#container");
+const resetButton = document.querySelector("#reset");
 
 function makeRows(rows, cols) {
+  container.innerHTML = "";
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
   for (let index = 0; index < rows * cols; index++) {
     const cell = document.createElement("div");
     cell.classList.add("grid-item");
-    cell.innerText = index + 1;
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor = "black";
+    });
     container.appendChild(cell);
   }
 }
-makeRows(16,16);
+
+resetButton.addEventListener("click", () => {
+  const gridNumber = parseInt(prompt("What grid you want?"));
+  if (!isNaN(gridNumber)) {
+    makeRows(gridNumber, gridNumber);
+  } else {
+    alert("You didn't add number");
+  }
+});
+
+makeRows(16, 16);
